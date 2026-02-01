7b91ffdbc08cc35c1224e4833ee9871059a5c677
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-02-01 20:56:51 +0500

    Drone

--- src/java/edu/narxoz/galactic/drones/Drone.java
+++ src/java/edu/narxoz/galactic/drones/Drone.java
@@ -1,4 +1,29 @@
 package java.edu.narxoz.galactic.drones;
 
-public class Drone {
+public abstract class Drone {
+    private String id;
+    private DroneStatus status;
+    private double maxPayloadKg;
+
+    protected Drone(String id, double maxPayloadKg){
+        if(maxPayloadKg <=0){
+            throw new IllegalArgumentException();
+        }
+        this.id = id;
+        this.maxPayloadKg = maxPayloadKg;
+        this.status = DroneStatus.IDLE;
+    }
+    public String getId(){
+        return id;
+    }
+    public DroneStatus getStatus(){
+        return status;
+    }
+    public double getMaxPayloadKg(){
+        return maxPayloadKg;
+    }
+    protected void setStatus(DroneStatus status){
+        this.status = status;
+    }
+    public abstract double speedKmPerMin();
 }
