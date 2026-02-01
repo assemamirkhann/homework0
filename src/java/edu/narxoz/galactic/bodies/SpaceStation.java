c5b9d2341b3f20906079830f1e7e5416d6e89fc1
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-02-01 20:46:50 +0500

    SpaceStation

--- src/java/edu/narxoz/galactic/bodies/SpaceStation.java
+++ src/java/edu/narxoz/galactic/bodies/SpaceStation.java
@@ -1,4 +1,15 @@
 package java.edu.narxoz.galactic.bodies;
 
-public class SpaceStation {
+public class SpaceStation extends CelestialBody{
+
+    private int level;
+
+    public SpaceStation(String name, double x, double y, int level){
+        super(name, x, y);
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
